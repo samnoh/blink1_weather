370cd2533b6042ff3a528353ea0578cf374abbb1
Sam Noh <samnoh21@outlook.com>
2021-01-26 22:44:50 +1300

    .

---
+++ api.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+
+const lat = '-36.8509';
+const lon = '174.7645';
+const APIKey = process.env.API_KEY;
+const APIOneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+const parts = ['current', 'minutely', 'hourly', 'daily', 'alerts'];
+
+function _getExcludePartsByType(type) {
+    return type ? parts.filter((part) => part !== type) : '';
+}
+
+async function _getWeatherAPIData(type) {
+    try {
+        const exclude = _getExcludePartsByType(type);
+        const response = await axios.get(`${APIOneCall}&exclude=${exclude}`);
+
+        return type ? response.data[type] : response.data;
+    } catch (error) {
+        console.error(`Type: ${type}\n${error}`);
+    }
+}
+
+function getAllWeather() {
+    return _getWeatherAPIData();
+}
+
+function getCurrentWeather() {
+    return _getWeatherAPIData(parts[0]);
+}
+
+function getMinutelyWeather() {
+    return _getWeatherAPIData(parts[1]);
+}
+
+function getHourlyWeather() {
+    return _getWeatherAPIData(parts[2]);
+}
+
+function getDailyWeather() {
+    return _getWeatherAPIData(parts[3]);
+}
+
+function getWeatherAlerts() {
+    return _getWeatherAPIData(parts[4]);
+}
+
+module.exports = {
+    getAllWeather,
+    getCurrentWeather,
+    getMinutelyWeather,
+    getHourlyWeather,
+    getDailyWeather,
+    getWeatherAlerts,
+};
--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+const dotenv = require('dotenv');
 const Blink1 = require('node-blink1');
 
+dotenv.config();
+
+const api = require('./api');
 const RGB = require('./colors');
 
 const blink1 = new Blink1();
 
-blink1.setRGB(...RGB.red, () => {
-    console.log('hello');
-});
+// blink1.setRGB(...RGB.blue, () => {
+//     console.log('hello');
+// });
+
+// blink1.off();
+
+async function main() {
+    const { uvi } = await api.getCurrentWeather();
+}
+
+main();
