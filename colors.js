cd18402e4c33dca0ce20443223c288ceac424755
Sam Noh <samnoh21@outlook.com>
2021-01-26 22:57:43 +1300

    add uvi conditions

--- colors.js
+++ colors.js
@@ -1,9 +1,15 @@
 const red = [255, 0, 0];
 const green = [0, 255, 0];
 const blue = [0, 0, 255];
+const yellow = [255, 255, 0];
+const orange = [255, 165, 0];
+const purple = [128, 0, 128];
 
 module.exports = {
     red,
     green,
     blue,
+    yellow,
+    orange,
+    purple,
 };
--- main.js
+++ main.js
@@ -6,16 +6,32 @@ dotenv.config();
 const api = require('./api');
 const RGB = require('./colors');
 
-const blink1 = new Blink1();
-
-// blink1.setRGB(...RGB.blue, () => {
-//     console.log('hello');
-// });
-
-// blink1.off();
+const myBlink = new Blink1();
 
 async function main() {
-    const { uvi } = await api.getCurrentWeather();
+    let { uvi } = await api.getCurrentWeather();
+    uvi = 3;
+    switch (uvi) {
+        case 0:
+        case 1:
+        case 2:
+            return myBlink.off(); // low
+        case 3:
+        case 4:
+        case 5:
+            return myBlink.setRGB(...RGB.yellow); // moderate
+        case 6:
+        case 7:
+            return myBlink.setRGB(...RGB.orange); // high
+        case 8:
+        case 9:
+        case 10:
+            return myBlink.setRGB(...RGB.red); // very high
+        case 11:
+            return myBlink.setRGB(...RGB.purple); // extreme
+        default:
+            return myBlink.off();
+    }
 }
 
 main();
